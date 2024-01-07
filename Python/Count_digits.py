406864b8444a1903f2a9b2564fc0bf7a88a0341b
Prakhar Vaish <73419181+VAISHPrakhar@users.noreply.github.com>
2024-01-07 13:35:18 +0530

    Update Count_digits.py

--- Python/Count_digits.py
+++ Python/Count_digits.py
@@ -1,5 +1,2 @@
-#a very simple trick to count digits, this can be used in competetive prgramming and also improves complexity
-import math
-n = int(input())
-#this is our simple trick function
-print(math.ceil(math.log10( n )))
+def count_digits(n):
+    print(len(str(n).replace('-', '').replace('.', '')))
